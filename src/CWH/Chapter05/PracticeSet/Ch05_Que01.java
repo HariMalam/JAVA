194f93744455b8fa25bf789cd9858629f8a7ef03
Malam Hari <malamharid86@gmail.com>
2023-03-14 12:38:49 +0530

    Initial commit

--- src/CWH/Chapter05/PracticeSet/Ch05_Que01.java
+++ src/CWH/Chapter05/PracticeSet/Ch05_Que01.java
@@ -1,4 +1,24 @@
 package CWH.Chapter05.PracticeSet;
+import java.util.Scanner;
+
+// Question 1 : Write a java Program to print the following pattern.
+//              * * * * *
+//              * * * *
+//              * * *
+//              * *
+//              *
 
 public class Ch05_Que01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Value of n : ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++){
+            for (int j = n; j > i; j--){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
 }
--- src/CWH/Chapter05/PracticeSet/Ch05_Que02.java
+++ src/CWH/Chapter05/PracticeSet/Ch05_Que02.java
@@ -1,4 +1,18 @@
 package CWH.Chapter05.PracticeSet;
+import java.util.Scanner;
+
+//Question 2 : Write a java program to sum first even numbers using while loop
 
 public class Ch05_Que02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Value of n : ");
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++){
+            sum = sum + (2*i);
+        }
+        System.out.println("The Sum of first "+n+ " even numbers is " +sum);
+    }
 }
