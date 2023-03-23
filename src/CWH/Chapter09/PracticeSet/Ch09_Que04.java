fb31c2a789db9ef60dce55ccab528d6ea62e4e4a
Malam Hari <malamharid86@gmail.com>
2023-03-23 13:39:26 +0530

    Initial commit

--- src/CWH/Chapter09/PracticeSet/Ch09_Que04.java
+++ src/CWH/Chapter09/PracticeSet/Ch09_Que04.java
@@ -2,5 +2,36 @@ package CWH.Chapter09.PracticeSet;
 
 // Question 04 : Overload a constructor used to initialize a rectangle of length 4 and breadth 5 for using custom parameters
 
+class Rectangle{
+    private int length;
+    private int breadth;
+
+    public Rectangle() {
+        this.length = 4;
+        this.breadth = 5;
+    }
+
+    public Rectangle(int length, int breadth) {
+        this.length = length;
+        this.breadth = breadth;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public int getBreadth() {
+        return breadth;
+    }
+}
+
 public class Ch09_Que04 {
+    public static void main(String[] args) {
+        Rectangle myRectangle = new Rectangle();
+        System.out.println(myRectangle.getBreadth());
+        System.out.println(myRectangle.getLength());
+        Rectangle myRectangle2 = new Rectangle(10,20);
+        System.out.println(myRectangle2.getLength());
+        System.out.println(myRectangle2.getBreadth());
+    }
 }
